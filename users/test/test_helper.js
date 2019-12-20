f689fe72a4ea79171e2550713bc6bdd82a19eeff
rehan712 <rehanbhatti712@gmail.com>
2019-12-19 23:56:25 -0800

    create test created

--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose
-  .connect("mongodb://localhost/user_test")
+mongoose.connect("mongodb://localhost/user_test");
+mongoose.connection
   .once("open", () => console.log("Good to go!"))
   .on("error", error => console.warn("Error", error));
